d5d1a66e12847a0bf3d5123957b2f0590a91e7af
John Smit <37273545+Sambisted@users.noreply.github.com>
2019-08-19 19:06:12 +0300

    added registration

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 import './pages/today.dart';
 import './pages/month.dart';
 import './pages/settings.dart';
+import 'ui/loginPage.dart';
 import 'package:date_utils/date_utils.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 
 void main(){
-  new MaterialApp(debugShowCheckedModeBanner: false,);
+  new MaterialApp( theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => LoginPage(),
+        '/todoscreen': (BuildContext context) => ToDayPage(),
+        
+      },
+      debugShowCheckedModeBanner: false,);
   initializeDateFormatting().then((_)=> runApp(MyApp()));
   
 } 
