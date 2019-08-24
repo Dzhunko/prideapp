000fbab19cb4e3dbdd754bc761cdaa58e437fb8b
Dzhunko <52957120+Dzhunko@users.noreply.github.com>
2019-08-24 15:31:33 +0300

    123

--- lib/ui/loginPage.dart
+++ lib/ui/loginPage.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:prideapp/pages/today.dart';
 import 'package:shared_preferences/shared_preferences.dart';
