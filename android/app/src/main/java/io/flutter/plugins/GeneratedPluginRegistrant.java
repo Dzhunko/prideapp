14f3cfe245b70e9791e62cef1061dc7acdad13d1
Dzhunko <52957120+Dzhunko@users.noreply.github.com>
2019-08-25 20:19:16 +0300

    added auth

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,7 +1,13 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin;
+import io.flutter.plugins.firebaseauth.FirebaseAuthPlugin;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+import io.github.ponnamkarthik.toast.fluttertoast.FluttertoastPlugin;
+import io.flutter.plugins.googlesignin.GoogleSignInPlugin;
 import io.flutter.plugins.pathprovider.PathProviderPlugin;
+import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin;
 import com.tekartik.sqflite.SqflitePlugin;
 
 /**
@@ -12,7 +18,13 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    CloudFirestorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.cloudfirestore.CloudFirestorePlugin"));
+    FirebaseAuthPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebaseauth.FirebaseAuthPlugin"));
+    FirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FirebaseCorePlugin"));
+    FluttertoastPlugin.registerWith(registry.registrarFor("io.github.ponnamkarthik.toast.fluttertoast.FluttertoastPlugin"));
+    GoogleSignInPlugin.registerWith(registry.registrarFor("io.flutter.plugins.googlesignin.GoogleSignInPlugin"));
     PathProviderPlugin.registerWith(registry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+    SharedPreferencesPlugin.registerWith(registry.registrarFor("io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin"));
     SqflitePlugin.registerWith(registry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,143 @@
 import 'package:flutter/material.dart';
-import './pages/today.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+//Pages
+import './pages/home.dart';
 import './pages/month.dart';
 import './pages/settings.dart';
+
 import 'package:date_utils/date_utils.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:bmnav/bmnav.dart' as bmnav;
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 
+//Firebase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+void main() {
+  new MaterialApp(
+    debugShowCheckedModeBanner: false,
+  );
+  initializeDateFormatting().then((_) => runApp(MyApp()));
+}
 
-void main(){
-  new MaterialApp(debugShowCheckedModeBanner: false,);
-  initializeDateFormatting().then((_)=> runApp(MyApp()));
-  
-} 
+class SignIn extends StatefulWidget {
+  @override
+  SignInState createState() => SignInState();
+}
 
+class SignInState extends State<SignIn> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  bool isLoading = false;
+  bool isLogged = false;
+
+  SharedPreferences prefs;
+  FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignIn();
+  }
+
+  void isSignIn() async {
+    setState(() {
+      isLoading = true;
+    });
+    prefs = await SharedPreferences.getInstance();
+
+    isLogged = await _googleSignIn.isSignedIn();
+    if (isLoading) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SettingsPageState()));
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<Null> handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    final FirebaseUser firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+
+    if (firebaseUser != null) {
+      final QuerySnapshot result = await Firestore.instance
+          .collection('user')
+          .where('id', isEqualTo: firebaseUser.uid)
+          .getDocuments();
+
+      List<DocumentSnapshot> documents = result.documents;
+      if(documents.length == 0) {
+        Firestore.instance.collection('user').document(firebaseUser.uid).setData({
+          'id': firebaseUser.uid,
+          'username': firebaseUser.displayName,
+          'photoUrl': firebaseUser.photoUrl
+        });
+
+        //Data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('username', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoUrl);
+      }
+      else {
+        await prefs.setString('id', documents[0]['id']);
+        await prefs.setString('username', documents[0]['username']);
+        await prefs.setString('photoUrl', documents[0]['photoUrl']);
+      }
+      Fluttertoast.showToast(
+        msg: "Sign in success"
+      );
+      setState(() {
+              isLoading = false;
+            });
+      Navigator.push(context, MaterialPageRoute(builder: (context)=> SettingsPageState()));
+    }
+    else {
+      Fluttertoast.showToast(
+        msg: "Sign in fail"
+      );
+      setState(() {
+              isLoading = false;
+            });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {}
+}
 
 class MyApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      
-      return MyAppState();
-    }
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
-  
+
 class MyAppState extends State<MyApp> {
   int _selectedPage = 0;
   Widget currentScreen = ToDayPage();
-  final _pageTitle=[
+  final _pageTitle = [
     'Today',
     'Month',
     'Settings',
   ];
-  final _pageOptions= [
+  final _pageOptions = [
     //ToDayPageState(),
     ToDayPage(),
     CalendarPageState(),
@@ -57,25 +162,29 @@ class MyAppState extends State<MyApp> {
           child: ListView(),
         ),
         backgroundColor: Colors.white,
-        body:  _pageOptions[_selectedPage],
+        body: _pageOptions[_selectedPage],
         bottomNavigationBar: bmnav.BottomNav(
           index: _selectedPage,
           onTap: (i) {
             setState(() {
               _selectedPage = i;
               currentScreen = _pageOptions[i];
-                        });
+            });
           },
-			     labelStyle: bmnav.LabelStyle(showOnSelect: true, onSelectTextStyle: TextStyle(color: Colors.amber)),
-           iconStyle: bmnav.IconStyle(onSelectSize: 36.0, color: Colors.black, onSelectColor: Colors.amber, size: 32.0),
-			items: [
-				bmnav.BottomNavItem(Icons.home, label: 'Home'),
-				bmnav.BottomNavItem(Icons.calendar_today, label: 'Calendar'),
-				bmnav.BottomNavItem(Icons.settings, label: 'Settings'),				
-			],
-		),
-	
-        
+          labelStyle: bmnav.LabelStyle(
+              showOnSelect: true,
+              onSelectTextStyle: TextStyle(color: Colors.amber)),
+          iconStyle: bmnav.IconStyle(
+              onSelectSize: 36.0,
+              color: Colors.black,
+              onSelectColor: Colors.amber,
+              size: 32.0),
+          items: [
+            bmnav.BottomNavItem(Icons.home, label: 'Home'),
+            bmnav.BottomNavItem(Icons.calendar_today, label: 'Calendar'),
+            bmnav.BottomNavItem(Icons.settings, label: 'Settings'),
+          ],
+        ),
       ),
     );
   }
